1ba30099beab5add9e959d09e628647eb940d5b3
Alexandr Berezhnoy <berezhnoya23@gmail.com>
2019-09-26 17:18:27 +0300

    refactor

--- server/services/User.js
+++ server/services/User.js
@@ -2,10 +2,11 @@ import UserModel from '../models/User';
 import { genRandomString, getHmac } from '../utils/hash';
 
 const { DB_SALT } = process.env;
+const salt = genRandomString(32);
 
 export const createUser = (email, password) =>
   UserModel.create({
     email,
-    passwordHash: getHmac(password, genRandomString(32) + DB_SALT),
-    passwordSalt: genRandomString(32),
+    passwordHash: getHmac(password, salt + DB_SALT),
+    passwordSalt: salt,
   });
