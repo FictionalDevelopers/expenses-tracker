349fc0782255b435cadc5c89e92b7a09fb0655cc
Alexandr Berezhnoy <berezhnoya23@gmail.com>
2019-09-26 17:21:57 +0300

    fix salt

--- server/services/User.js
+++ server/services/User.js
@@ -2,11 +2,13 @@ import UserModel from '../models/User';
 import { genRandomString, getHmac } from '../utils/hash';
 
 const { DB_SALT } = process.env;
-const salt = genRandomString(32);
 
-export const createUser = (email, password) =>
-  UserModel.create({
+export const createUser = (email, password) => {
+  const salt = genRandomString(32);
+
+  return UserModel.create({
     email,
     passwordHash: getHmac(password, salt + DB_SALT),
     passwordSalt: salt,
   });
+};
