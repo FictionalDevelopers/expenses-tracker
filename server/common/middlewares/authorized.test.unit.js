7f93f1152dfc0e3a0bf7ffff0a6f60960199512b
dmitry.honcharuk <dmitry.honcharuk@gmail.com>
2019-12-15 17:58:14 +0200

    configured testing github action

--- server/common/middlewares/authorized.test.unit.js
+++ server/common/middlewares/authorized.test.unit.js
@@ -1,12 +1,20 @@
-import { STATUS_CODES } from 'http';
 import faker from 'faker';
 
 import { TOKEN_COOKIE_NAME } from '@common/config';
 
 import { AuthService } from '@components/auth';
+import { ApiError, UnauthorizedError } from '@common/Errors';
+
 import authorized from './authorized';
 
 jest.mock('@components/auth');
+
+jest.mock('@common/Errors/UnauthorizedError', () => {
+  return args => args;
+});
+jest.mock('@common/Errors/ApiError', () => {
+  return args => args;
+});
 
 const json = jest.fn(data => data);
 const status = jest.fn(() => ({
@@ -58,9 +66,8 @@ describe('common - middlewares - authorized', () => {
 
     authorized(req, res, next);
 
-    expect(res.status).toBeCalledTimes(1);
-    expect(res.status).toBeCalledWith(401);
-    expect(res.json).toBeCalledWith({ error: STATUS_CODES[401] });
+    expect(next).toBeCalledTimes(1);
+    expect(next).toBeCalledWith(new UnauthorizedError());
   });
 
   it('should respond with 400 if token is not valid', () => {
@@ -77,8 +84,7 @@ describe('common - middlewares - authorized', () => {
 
     authorized(req, res, next);
 
-    expect(res.status).toBeCalledTimes(1);
-    expect(res.status).toBeCalledWith(400);
-    expect(res.json).toBeCalledWith({ error: errorMessage });
+    expect(next).toBeCalledTimes(1);
+    expect(next).toBeCalledWith(new ApiError(errorMessage));
   });
 });
